139ccd24370bb2278589d2739535933180dcd2d5
xulin <xulin@lattebank.com>
2022-12-28 22:23:59 +0800

    feat(demo-multi-datasource):多数据源demo模块

---
+++ demo-multi-datasource/src/main/java/indi/latch/demo/multidatasource/MultiDataSourceApplication.java
@@ -0,0 +1,20 @@
+package indi.latch.demo.multidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Title: MultiDataSourceApplication
+ * Description:
+ * Copyright: 数禾科技 Copyright(c) 2022/12/28
+ * Encoding: UNIX UTF-8
+ *
+ * @author 徐林
+ */
+@SpringBootApplication
+public class MultiDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDataSourceApplication.class, args);
+    }
+}
